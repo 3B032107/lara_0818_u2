7b60e5b697bfd5013fca5357ad36eb93e54f07d2
3b032107 <3b032107@gm.student.ncut.edu.tw>
2023-10-30 12:33:17 +0800

    初始提交

--- config/app.php
+++ config/app.php
@@ -163,7 +163,6 @@ return [
         /*
          * Application Service Providers...
          */
-        recca0120\LaravelTracy\ServiceProvider::class,
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
