83b2238c7b6dab72db1041d50e9e49fce96b98ad
3b032107 <3b032107@gm.student.ncut.edu.tw>
2023-10-30 13:03:39 +0800

    建立子樣板

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,15 @@
+
+<html>
+    <head>
+        <title>index.blade.php</title>
+    </head>
+    <body>
+    @extends('layouts.master')
+    @section('title','Home')
+    @section('content')
+        <h1>Home</h1>
+        <p>This is my body content.</p>
+    @endsection
+    </body>
+</html>
+
--- resources/views/home/index.php
+++
@@ -1,6 +0,0 @@
-
-<html>
-<title>home.html</title>
-<body><p>This is my lara_0818_u2 web.</p></body>
-</html>
-
