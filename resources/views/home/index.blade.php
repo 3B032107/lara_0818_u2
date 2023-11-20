01c45972026548626add2f907cb1d4aae246b269
3b032107 <3b032107@gm.student.ncut.edu.tw>
2023-11-21 07:47:24 +0800

    修正 在home/index.blade.php裡不需要HTML架構的部分

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,15 +1,8 @@
-
-<html>
-    <head>
-        <title>index.blade.php</title>
-    </head>
-    <body>
     @extends('layouts.master')
     @section('title','Home')
     @section('content')
         <h1>Home</h1>
         <p>This is my body content.</p>
     @endsection
-    </body>
-</html>
+
 
